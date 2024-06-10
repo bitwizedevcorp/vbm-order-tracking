a2b968c5b798603287a528d89160ae504df4e4a9
Voicu Bogdan <voicubogdan72@gmail.com>
2024-06-10 19:10:46 +0300

    add models in prisma

---
+++ lib/prisma.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default prisma;
+// host = 192.168.1.4
+// port = 3306
+// user = softagro
+// db = pacrumania
+// pw = Acme.1189
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,37 @@
-const Home = () => {
+import prisma from "../../lib/prisma";
+
+const Home = async () => {
+  async function getClientFromDbWithState() {
+    try {
+      const data = await prisma.tb_orden.findMany({
+        where: {
+          state: 6,
+        },
+      });
+
+      return data.map((order) => ({
+        cliente: order.cliente,
+        destino: order.destino,
+        fecha_despacho: order.fecha_despacho,
+      }));
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      return [];
+    }
+  }
+
+  const data = await getClientFromDbWithState();
+  console.log(data);
   return (
     <div>
-      <section>Order details</section>
+      <section className="flex items-center justify-center h-screen bg-gray-100 text-center p-4">
+        <div>
+          <h1 className="text-2xl font-bold">
+            Client and destination delivery, day_depature
+          </h1>
+        </div>
+      </section>
+      <div></div>
     </div>
   );
 };
